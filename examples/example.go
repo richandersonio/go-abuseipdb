35aaa2d346313b3e9671d2039037b99e64612010
Rich Anderson <richanderson00@gmail.com>
2020-03-21 23:24:08 -0400

    fix small bug

--- examples/example.go
+++ examples/example.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/richandersonio/go-abuseipdb/abuseipdb"
 )
 
-func main() {
-
+func check(ip string) {
 	apikey := os.Getenv("ABUSEIPDB_API_KEY")
-	resp, err := abuseipdb.CheckIP(apikey, "144.91.79.5")
-	if err == nil {
+	resp, err := abuseipdb.CheckIP(apikey, ip)
+	if err != nil {
 
 		fmt.Println("error checking ip: ", err)
+		return
 	}
 
 	fmt.Println("IPAddress:", resp.IPAddress)
@@ -28,4 +30,24 @@ func main() {
 	fmt.Println("ISP:", resp.ISP)
 	fmt.Println("Domain:", resp.Domain)
 	fmt.Println("LastReportedAt:", resp.LastReportedAt)
+}
+
+func main() {
+
+	fmt.Println("Checking IP Address...")
+	check("144.91.79.5")
+
+	for {
+		fmt.Println()
+		fmt.Print("Enter another IP address to check (just hit enter to quit):")
+		reader := bufio.NewReader(os.Stdin)
+		ans, _ := reader.ReadString('\n')
+		ans = strings.TrimRight(ans, "\n")
+		if ans == "" {
+			return
+		}
+		fmt.Println("Checking IP Address...")
+		check(ans)
+	}
+
 }
